e6f2e79f9d21961d54e6df5329ce85aa9509d67b
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-19 17:44:36 -0600

    Use app properties and pass name to OutputService
    Load greeting and name from application.properties via @PropertySource and @Value. Added app.name and app.greeting properties. Updated ApplicationConfig to inject name and pass it into the OutputService bean. Modified OutputService to accept and store the name and to expose a no-arg generateOutput(), and updated Application to call generateOutput() in the loop.

--- src/main/java/com/frankmoley/lil/fid/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/ApplicationConfig.java
@@ -5,25 +5,30 @@ import com.frankmoley.lil.fid.service.OutputService;
 import com.frankmoley.lil.fid.service.TimeService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application.properties")
 public class ApplicationConfig {
 
-  @Value("Hello")
-  private String greeting;
+    @Value("${app.greeting}")
+    private String greeting;
 
-  @Bean
-  public TimeService timeService(){
-    return new TimeService(true);
-  }
+    @Value("${app.name}")
+    private String name;
 
-  @Bean
-  public OutputService outputService(GreetingService greetingService, TimeService timeService){
-    return new OutputService(greetingService, timeService);
-  }
+    @Bean
+    public TimeService timeService() {
+        return new TimeService(true);
+    }
 
-  @Bean
-  public GreetingService greetingService(){
-    return new GreetingService(greeting);
-  }
+    @Bean
+    public OutputService outputService(GreetingService greetingService, TimeService timeService) {
+        return new OutputService(greetingService, timeService, name);
+    }
+
+    @Bean
+    public GreetingService greetingService() {
+        return new GreetingService(greeting);
+    }
 
 }
